1259a45a7446fd59a947df36e90f34326e4c2740
pabikshyathapa <pabikshyathapa@gmail.com>
2024-06-26 09:59:55 +0545

    workflow

--- app/src/main/java/com/example/taskcrud/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskcrud/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taskcrud.R
